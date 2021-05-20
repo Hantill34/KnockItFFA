e92883cdb13451b470b5fd437b773d09a988f25a
Tafelbirne <thegamefactory13@gmail.com>
2021-05-20 16:36:08 +0200

    Grabbling Hook Boostpads

--- src/net/problemzone/knockit/boostpads/moveListener.java
+++ src/net/problemzone/knockit/boostpads/moveListener.java
@@ -1,6 +1,6 @@
 package net.problemzone.knockit.boostpads;
 
-import org.bukkit.Effect;
+
 import org.bukkit.Material;
 import org.bukkit.Sound;
 import org.bukkit.entity.Player;
@@ -16,7 +16,7 @@ public class moveListener implements Listener {
         Player player = event.getPlayer();
         if(player.getLocation().getBlock().getType() == Material.LIGHT_WEIGHTED_PRESSURE_PLATE){
             if(player.getLocation().subtract(0.0D, 1.0D, 0.0D).getBlock().getType() == Material.REDSTONE_BLOCK) {
-                Vector v = player.getLocation().getDirection().multiply(3D).setY(1);
+                Vector v = player.getLocation().getDirection().multiply(3D).setY(1.5);
                 player.setVelocity(v);
                 player.playSound(player.getLocation(), Sound.ENTITY_ENDER_DRAGON_FLAP, 1, 1);
             }
